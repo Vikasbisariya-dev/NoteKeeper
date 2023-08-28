56021ff4061038996a85307ed74e433f4efffd2c
Vikas bisariya <vikasbisariya20@gmail.com>
2023-08-28 21:44:34 +0530

    notekeeper resourses uploaded

---
+++ src/Components/App.js
@@ -0,0 +1,54 @@
+import React,{useState} from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+// import Notes from "../notes";
+import CreateArea from "./CreateTextArea";
+
+// function createnotes(noteitems){
+//     return(
+//         <Note
+//         key={noteitems.key}
+//         title={noteitems.title}
+//         content={noteitems.content}
+//         />
+//     )
+// }
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
